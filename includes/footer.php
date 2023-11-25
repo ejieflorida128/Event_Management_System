38b06b06d32a7e579bbe4f21abc2cb20049ad420
ejieflorida128 <ejieflorida128@gmail.com>
2023-11-25 16:39:38 +0800

    [ejie] nag add ug footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel = "stylesheet" href = "../includes_style/footer.css">
 </head>
 <body>
-    
+    <div class = "footer">
+        Event Management System - Pink Lunary [BSIT] @ 2023
+    </div>
 </body>
 </html>
