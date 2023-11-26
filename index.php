77aa2f275652e2f4a7398e0d281e986ccbc7c944
ejieflorida128 <ejieflorida128@gmail.com>
2023-11-26 09:01:45 +0800

    [ejie ge butangan ug see icon ang password sa log in]

--- index.php
+++ index.php
@@ -48,17 +48,24 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login & Registration Form</title>
- 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="index.css">
 </head>
 <body>
   <div class="container">
-    <input type="checkbox" id="check">
+    <input type="checkbox" id="seePass" style = "display: none;">
     <div class="login form">
       <header>Login</header>
-      <form action="index.php" method = "post">
-        <input type="text" placeholder="Username" name = "username">
-        <input type="password" placeholder="Password" name = "password">       
+      <form action="index.php" method="post">
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" id="password" placeholder="Password" name="password">
+        <label for="seePass" class="seePass">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+            <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588M5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+            <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+          </svg>
+        </label>
         <input type="submit" class="button" value="Login">
       </form>
       <div class="signup">
@@ -67,7 +74,21 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         </span>
       </div>
     </div>
-  
   </div>
+
+  <script>
+    document.getElementById('seePass').addEventListener('change', function () {
+        var passwordInput = document.getElementById('password');
+        if (this.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+  </script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 </html>
+
