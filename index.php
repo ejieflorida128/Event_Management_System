c913d30156b7adeba73e796e452c3ec355d99d4d
ejieflorida128 <ejieflorida128@gmail.com>
2023-11-26 09:38:29 +0800

    [ejie] adding user icon in index.php

--- index.php
+++ index.php
@@ -59,6 +59,11 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
       <header>Login</header>
       <form action="index.php" method="post">
         <input type="text" placeholder="Username" name="username">
+        <label for="user" class="user">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+        </svg>
+        </label>
         <input type="password" id="password" placeholder="Password" name="password">
         <label for="seePass" class="seePass">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
