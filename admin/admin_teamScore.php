39f2f672ea42098d452cd1580e163dfbcca068bc
raymart28 <151854242+raymart28@users.noreply.github.com>
2023-11-29 17:56:32 +0800

    [lacre] ahu ge change ang modal name

--- admin/admin_teamScore.php
+++ admin/admin_teamScore.php
@@ -35,7 +35,7 @@ include("../conn/connection.php");
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">View Score</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">View Score Board</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
